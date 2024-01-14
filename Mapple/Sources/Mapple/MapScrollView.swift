599c4162edb202b21bed49c0e77c43434f3d71b5
Pavel Alexeev <pasha.alexeev@gmail.com>
2024-01-15 00:18:48 +0200

    longPressDuration is another variable

--- Mapple/Sources/Mapple/MapScrollView.swift
+++ Mapple/Sources/Mapple/MapScrollView.swift
@@ -55,6 +55,7 @@ public class MapScrollView: UIView {
 	private var doubleTapDragZooming = false
 	private var doubleTapDragZoomCenter: CGPoint = .zero
 	private let doubleTapDragZoomDelay: TimeInterval = 0.26
+	private let longPressDuration: TimeInterval = 0.26
 	private let doubleTapDragZoomSpeed = 0.012
 	private let tapHoldDuration = 0.25
 	private var twoFingerTapTimestamp: TimeInterval?
@@ -211,7 +212,7 @@ public class MapScrollView: UIView {
 					}
 					onLongPress(point: previousCentroid ?? centroid)
 				})
-				DispatchQueue.main.asyncAfter(deadline: .now() + doubleTapDragZoomDelay + 0.01, execute: longPressWorkItem!)
+				DispatchQueue.main.asyncAfter(deadline: .now() + longPressDuration + 0.01, execute: longPressWorkItem!)
 			}
 		}
 		
