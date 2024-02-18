b043ba0b69a202096604c08c65de88160d2ad575
Pavel Alexeev <pasha.alexeev@gmail.com>
2024-02-18 15:38:01 +0200

    Methods for performance improvements

--- Mapple/Sources/Mapple/MapProjection.swift
+++ Mapple/Sources/Mapple/MapProjection.swift
@@ -31,7 +31,6 @@ public struct SphericalMercator: Projection {
 		let x = radius * coordinates.longitude.radians - falseEasting
 		let y = ((radius / 2.0 * log((1.0 + sin(coordinates.latitude.radians)) / (1.0 - sin(coordinates.latitude.radians)))) - falseNorthing) * -1
 		
-		
 		return Point(x: x, y: y)
 	}
 	
@@ -45,6 +44,30 @@ public struct SphericalMercator: Projection {
 			latitude: -(2 * atan(exp((point.y + falseEasting) / radius)) - (.pi / 2)).degrees,
 			longitude: Double(point.x - falseNorthing).degrees / radius
 		)
+	}
+}
+
+extension SphericalMercator {
+	public func zoomData(for zoom: Double) -> ZoomData {
+		let tiles = pow(2, zoom)
+		let circumference = tiles * Double(tileSize)
+		let radius = circumference / (2 * .pi)
+		let falseEasting = -1.0 * circumference / 2.0
+		let falseNorthing = circumference / 2.0
+		return ZoomData(radius: radius, falseEasting: falseEasting, falseNorthing: falseNorthing)
+	}
+	
+	public func point(withZoomData data: ZoomData, from coordinates: Coordinates) -> PointOffset {
+		let x = data.radius * coordinates.longitude.radians - data.falseEasting
+		let y = ((data.radius / 2.0 * log((1.0 + sin(coordinates.latitude.radians)) / (1.0 - sin(coordinates.latitude.radians)))) - data.falseNorthing) * -1
+		
+		return Point(x: x, y: y)
+	}
+	
+	public struct ZoomData {
+		let radius: Double
+		let falseEasting: Double
+		let falseNorthing: Double
 	}
 }
 
